3e523e0a147800ee4a37b4d4f59983f2c2640e87
tonykorycki <koryckiantoni@gmail.com>
2025-02-26 19:58:44 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -45,6 +45,6 @@ module tt_um_KHC_module (
     assign uio_out = 8'b0;  // Not used
     assign uio_oe = 8'b0;   // Set as inputs
     
-    // Handle unused inputs
-    wire _unused = &{ena, clk, rst_n, 1'b0};
+    // Handle unused inputs - fixed the syntax error in rst_n reference
+    wire unused = &{ena, clk, rst_n, 1'b0};
 endmodule
