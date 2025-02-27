49d1d41e862b72252e266b2430a8d92c29066095
tonykorycki <koryckiantoni@gmail.com>
2025-02-26 20:34:22 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,16 +1,19 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
 module tb ();
 
-  // Dump the signals to a VCD file
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Wire up the inputs and outputs
+  // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
   reg ena;
@@ -24,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Instantiate the module
-  tt_um_priority_encoder user_project (
+  // Instantiate the module with the correct name
+  tt_um_KHC_module user_project (
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
